f3e39ed2a2633d82eae43c499ac4454a19ef9276
Bálint Dózsa <balint.dozsa@devertix.com>
2025-08-26 10:59:44 +0200

    Fixes & add PHP 8.3 + 8.4

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 
-for v in "8.2" "8.1" "8.0" "7.4" "7.3" "7.2";do
-    cd $v && docker build -t ghcr.io/devertix/phpenv:$v . && cd ..
+for v in "8.4" "8.3" "8.2" "8.1";do # "8.0" "7.4" "7.3" "7.2";do
+    cd $v && docker buildx build --platform=linux/amd64 -t ghcr.io/devertix/phpenv:$v . && cd ..
 done
--- push.sh
+++ push.sh
@@ -1,4 +1,4 @@
 
-for v in "8.2" "8.1" "8.0" "7.4" "7.3" "7.2";do
+for v in "8.4" "8.3" "8.2" "8.1";do # "8.0" "7.4" "7.3" "7.2";do
     docker push ghcr.io/devertix/phpenv:$v
 done
